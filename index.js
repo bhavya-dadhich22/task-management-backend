ade6bf15fa59261420b06277b4df1a930f4fa3e4
Vijay Shanker Dadhich <vijay.dadhich09@gmail.com>
2025-04-12 10:07:03 -0500

    update package.json

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const express = require('express');
+const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -7,33 +7,27 @@ const taskRoutes = require("./routes/taskRoutes");
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
+app.get("/", function (req, res) {
+  res.send("Task Management Server Application!");
+});
 
-app.get('/', function (req, res) {
-  res.send('Task Management Server Application!')
-})
-
-console.log('MONGO_URI:', process.env.MONGO_URI);
-
-console.log('portNumber:', process.env.PORT);
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "task_management",
+  })
   .then(() => {
-    console.log("Connected to database111!");
+    console.log("Connected to database!");
     const portNumber = process.env.PORT || 8000;
-    console.log('portNumber:', portNumber);
     app.listen(portNumber, () => {
       console.log(`Server is running on port: ${portNumber}`);
     });
   })
   .catch((err) => {
-  console.log('MONGO_URI1:', process.env.MONGO_URI);
-  console.log('portNumber1:', process.env.PORT);
-  console.log("Connection failed:", err);
+    console.log("Connection failed:", err);
   });
 
 app.use("/api/tasks", taskRoutes);
-
-// const PORT = process.env.PORT || 6000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
